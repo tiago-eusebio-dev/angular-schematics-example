b50de72ca846b913173f20abfdee743cd2b83967
PRIMAVERA\Tiago.Eusebio <tiago.eusebio@primaverabss.com>
2021-06-17 18:16:21 +0100

    Add fake angular.json and app/app.module.ts so the schematics is runnable

---
+++ src/app/app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';import { TestThingModule } from './test-thing/test-thing.module'; 
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, TestThingModule],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
