f0df578018878835c811128b303c78268d644972
PRIMAVERA\Tiago.Eusebio <tiago.eusebio@primaverabss.com>
2021-06-18 12:38:58 +0100

    Fix Material components imports

--- src/order-wizard/files/__name@dasherize__.module.ts
+++ src/order-wizard/files/__name@dasherize__.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import {
-  MatButtonModule, MatCardModule, MatDatepickerModule, MatFormFieldModule,
-  MatInputModule, MatNativeDateModule, MatSelectModule, MatStepperModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatStepperModule } from '@angular/material/stepper';
 
 import { <%= classify(name) %>Component } from './<%= dasherize(name) %>.component';
 import { <%= classify(name) %>Service } from './<%= dasherize(name) %>.service';
